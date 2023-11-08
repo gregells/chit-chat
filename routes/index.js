c45dbfd211696edfbb2a7bcd0b830a21c8142afe
Greg Ells <gregells@gmail.com>
2023-11-08 14:22:09 +1300

    CSS changes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chit-Chat' });
+  res.render('index', { title: 'Chit Chat' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // Optional:
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/posts',
+    // Change to what's best for your app:
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+      // Change path for you 'landing' page:
+    res.redirect('/posts');
+  });
 });
 
 module.exports = router;
