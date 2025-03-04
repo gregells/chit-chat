8c0453488a0dfa1c1f94909d6d224bc75e771460
Greg Ells <gregells@gmail.com>
2025-03-03 19:52:09 -0500

    Fix indentation on JS files

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,79 +1,79 @@
 const Post = require('../models/post');
 
 module.exports = {
-    index,
-    show,
-    new: newPost,
-    create,
-    delete: deletePost,
-    edit,
-    update
+  index,
+  show,
+  new: newPost,
+  create,
+  delete: deletePost,
+  edit,
+  update
 };
 
 async function index(req, res) {
-    const posts = await Post.find({});
-    res.render('posts/index', { title: 'All Posts', posts });
+  const posts = await Post.find({});
+  res.render('posts/index', { title: 'All Posts', posts });
 }
 
 async function show(req, res) {
-    // Find the post and populate the comments:
-    const post = await Post.findById(req.params.id);
-    res.render('posts/show', { title: 'Post Details', post });
+  // Find the post and populate the comments:
+  const post = await Post.findById(req.params.id);
+  res.render('posts/show', { title: 'Post Details', post });
 }
 
 function newPost(req, res) {
-    // errorMsg if the create action fails
-    res.render('posts/new', { title: 'New Post', errorMsg: ''});
+  // errorMsg if the create action fails
+  res.render('posts/new', { title: 'New Post', errorMsg: ''});
 }
 
 async function create(req, res) {
-    console.log(req.body.postContent);
-    // Add the user information:
-    req.body.postAuthor = req.user._id;
-    req.body.postAuthorName = req.user.name;
-    req.body.postAuthorAvatar = req.user.avatar;
+  console.log(req.body.postContent);
+  // Add the user information:
+  req.body.postAuthor = req.user._id;
+  req.body.postAuthorName = req.user.name;
+  req.body.postAuthorAvatar = req.user.avatar;
 
-    try {
-        const post = await Post.create(req.body);
-        // Redirect to the new posts show functionality:
-        res.redirect(`/posts/${ post._id }`);
-    } catch (err) {
-        // Give error message if unsuccesful:
-        console.log(err);
-        res.render('posts/new', { errorMsg: err.message, title: 'Error' });
-    }
+  try {
+    const post = await Post.create(req.body);
+    // Redirect to the new posts show functionality:
+    res.redirect(`/posts/${ post._id }`);
+  } catch (err) {
+    // Give error message if unsuccesful:
+    console.log(err);
+    res.render('posts/new', { errorMsg: err.message, title: 'Error' });
+  }
 }
 
 async function deletePost(req, res) {
-    const post = await Post.findById(req.params._id);
-    
-    try {
-        await Post.findByIdAndDelete(req.params.id);
-    } catch (err) {
-        console.log(err);
-        res.redirect('/posts');
-    }
-
-    // Redirect after CRUDing data:
+  const post = await Post.findById(req.params._id);
+  
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+  } catch (err) {
+    console.log(err);
     res.redirect('/posts');
+  }
+
+  // Redirect after CRUDing data:
+  res.redirect('/posts');
 }
 
 async function edit(req, res) {
-    const post = await Post.findById(req.params.id);
-    console.log(post);
+  const post = await Post.findById(req.params.id);
+  console.log(post);
 
-    // Render the prefilled form:
-    res.render('posts/edit', {
-        title: 'Edit Post',
-        post
-    });
+  // Render the prefilled form:
+  res.render('posts/edit', {
+    title: 'Edit Post',
+    post
+  });
 }
 
 async function update(req, res) {
-    const post = await Post.findById(req.params.id);
-    post.postContent = req.body.postContent;
-    await post.save();
+  const post = await Post.findById(req.params.id);
+  post.postContent = req.body.postContent;
+  await post.save();
 
-    // Redirect after CRUDing data:
-    res.redirect(`/posts/${ req.params.id }`);
+  // Redirect after CRUDing data:
+  res.redirect(`/posts/${ req.params.id }`);
 }
--- models/post.js
+++ models/post.js
@@ -3,46 +3,46 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    commentContent: {type: String, required: true},
-    commentAuthor: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    commentAuthorName: String,
-    commentAuthorAvatar: String,
-    commentThumbsUp: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    commentThumbsDown: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }]
+  commentContent: {type: String, required: true},
+  commentAuthor: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  commentAuthorName: String,
+  commentAuthorAvatar: String,
+  commentThumbsUp: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  commentThumbsDown: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 const postSchema = new Schema({
-    postContent: {type: String, required: true},
-    postAuthor: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    postAuthorName: String,
-    postAuthorAvatar: String,
-    comments: [commentSchema],
-    postThumbsUp: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    postThumbsDown: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }]
+  postContent: {type: String, required: true},
+  postAuthor: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  postAuthorName: String,
+  postAuthorAvatar: String,
+  comments: [commentSchema],
+  postThumbsUp: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  postThumbsDown: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 // Compile the Schema into a model and export it:
