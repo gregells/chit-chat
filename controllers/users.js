655b7b76078f846a85e5bc033329d2eb7f0a04b4
Greg Ells <gregells@gmail.com>
2025-03-07 20:18:45 -0500

    Add controller action for users index

---
+++ controllers/users.js
@@ -0,0 +1,10 @@
+const User = require('../models/user');
+
+module.exports = {
+  index,
+}
+
+async function index(req, res) {
+  const users = await User.find({});
+  res.render('users/index', { title: 'All Users', users });
+}
