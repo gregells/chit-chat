b078ee8a69806765405a4160b06732a368b8ce49
Greg Ells <gregells@gmail.com>
2025-03-07 20:18:11 -0500

    Add route for users index page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require('../controllers/users');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// All roots start with /users
+
+// GET /users
+router.get('/', usersController.index);
 
 module.exports = router;
