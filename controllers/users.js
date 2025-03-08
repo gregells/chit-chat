961e8c2eca618912b2580b216ec10759226c4ddc
Greg Ells <gregells@gmail.com>
2025-03-07 23:12:18 -0500

    Add the controller action to show a user's detail page

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,23 @@
 const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports = {
   index,
+  show
 }
 
 async function index(req, res) {
   const users = await User.find({});
   res.render('users/index', { title: 'All Users', users });
+}
+
+async function show(req, res) {
+  // Find the user:
+  const user = await User.findById(req.params.id);
+  // Find the user's posts:
+  const posts = await Post.find({
+    'postAuthor': req.params.id
+  });
+
+  res.render('users/show', { title: `${user.name}'s Posts`, user, posts });
 }
