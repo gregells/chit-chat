33626c24885ec4222365159a6359d25a67d7837f
Greg Ells <gregells@gmail.com>
2025-07-30 17:25:28 -0400

    Remove unused import

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/users');
-const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // All roots start with /users
 
